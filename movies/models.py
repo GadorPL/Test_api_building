19e5de488829a9225292d4f7be782d2ae0b1f0bd
Michał Hewelt <mhewelt9@gmail.com>
2022-10-31 15:38:08 +0100

    add ImageField

--- movies/models.py
+++ movies/models.py
@@ -10,4 +10,5 @@ class Movie(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     type = models.CharField(max_length=200, default='action')
+    image = models.ImageField(upload_to='images/', default='images/None/Noimg.jpg')
 
--- movies/serializers.py
+++ movies/serializers.py
@@ -3,6 +3,8 @@ from .models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None, use_url=True)
+
     class Meta:
         model = Movie
-        fields = ['id', 'name', 'duration', 'rating', 'type']
+        fields = ['id', 'name', 'duration', 'rating', 'type', 'image']
